ae1ad300f4c0254f203a4d2f2ecd8b325ec26f05
Nguyen Huu Hanh <hanh.nguyen081191@gmail.com>
2022-04-29 21:18:23 +0700

    check click explore

--- src/components/home/Group.js
+++ src/components/home/Group.js
@@ -8,8 +8,7 @@ import IcPool from '../../assets/images/home/ic_cream.svg';
 import IcFarm from '../../assets/images/home/ic_farm.svg';
 import IcStake from '../../assets/images/home/ic_piggy.svg';
 
-
-const Group = ({ exploreRef }) => {
+const Group = () => {
 
   return (
 
