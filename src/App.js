74acd3d79cc3d5217b7be7163dda9725f7ce789f
PTChinh <44965578+PTChinh@users.noreply.github.com>
2022-05-25 10:23:17 +0700

    Handle StakingPage UI

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const PoolPage = React.lazy(() => import('./pages/PoolPage'));
 const MarketPage = React.lazy(() => import('./pages/MarketPage'));
 const FarmPage = React.lazy(() => import('./pages/FarmPage'));
 const TradePage = React.lazy(() => import('./pages/TradePage'));
+const StakingPage = React.lazy(() => import('./pages/StakingPage'));
 
 function App() {
 
@@ -34,6 +35,8 @@ function App() {
           <Route path="/farm" element={<FarmPage/>} />
 
           <Route path="/trade" element={<TradePage />} />
+
+          <Route path="/stake" element={<StakingPage />} />
 
           <Route path="*" element={<Navigate to="/" />} />
 
---
+++ src/components/account/BtnConnectInPage.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { useDispatch } from "react-redux";
+
+import * as actions from '../../actions';
+
+const BtnConnectInPage = () => {
+
+    const [isConnecting, setIsConnecting] = useState(false);
+
+    const dispatch = useDispatch();
+
+    const connectWalletHandler = async () => {
+        if (!isConnecting) {
+            setIsConnecting(true)
+            await dispatch(actions.web3Connect(true)).then(() => {
+                setIsConnecting(false)
+            }).catch((e) => {
+                setIsConnecting(false)
+            });
+        }
+    }
+
+
+    return (
+        <>
+            <button
+                onClick={connectWalletHandler}
+                className="btn-veb h-10"
+                type="submit">
+                {isConnecting ? "Connecting..." : "Connect to a wallet"}
+            </button>
+        </>
+    )
+}
+
+export default BtnConnectInPage;
