d9adb22503fbb376fba5bf77584e20a6d791d1de
PTChinh <44965578+PTChinh@users.noreply.github.com>
2022-05-26 10:15:03 +0700

    Update MenuLink and loading action

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,51 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    container: {
-      padding: {
-        xs: '1rem',
-        sm: '1rem',
-        lg: '1rem',
-        xl: '2rem',
-        '2xl': '2rem',
-      },
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        container: {
+            padding: {
+                xs: '1rem',
+                sm: '1rem',
+                lg: '1rem',
+                xl: '2rem',
+                '2xl': '2rem',
+            },
+        },
+        screens: {
+            xs: '420px',
+            sm: '640px',
+            md: '768px',
+            lg: '1280px',
+            xl: '1440px',
+            '2xl': '1690px'
+        },
+        extend: {
+            fontFamily: {
+                montserrat: "'Montserrat', sans-serif",
+                poppins: "'Poppins', sans-serif",
+                sans_serif: "sans-serif",
+                sf_pro: "SF Pro Display",
+                nebula: "Nebula",
+                blank_space: "Blank Space"
+            }
+        },
+        colors: {
+            primary: "#02A4FF",
+            secondary: "#0FE3E3",
+            transparent: "transparent",
+            black: colors.black,
+            white: colors.white,
+            gray: colors.trueGray,
+            indigo: colors.indigo,
+            red: colors.red,
+            green: colors.green,
+            blue: colors.blue,
+            pink: colors.pink,
+            yellow: colors.yellow,
+        }
     },
-    screens: {
-      xs: '420px',
-      sm: '640px',
-      md: '768px',
-      lg: '1280px',
-      xl: '1440px',
-      '2xl': '1690px'
-    },
-    extend: {
-      fontFamily: {
-        montserrat: "'Montserrat', sans-serif",
-        poppins: "'Poppins', sans-serif",
-        sans_serif: "sans-serif",
-        sf_pro: "SF Pro Display",
-        nebula: "Nebula",
-        blank_space: "Blank Space"
-      }
-    },
-  },
-  plugins: [],
+    plugins: [],
 }
