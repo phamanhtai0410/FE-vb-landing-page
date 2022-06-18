d39f99f58349a0deb4d2c3db67339084a302d664
Khoa Nguyễn <khoanguyenanh56@gmail.com>
2022-06-18 11:29:18 +0700

    _stag_1_"Remove comming soon from all page for testing"

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import MainLayout from './layouts/MainLayout';
 import Page404 from './pages/Page404';
 
 const HomePage = React.lazy(() => import('./pages/HomePage'));
-const PoolPage = React.lazy(() => import('./pages/PoolPage'));
 const MarketPage = React.lazy(() => import('./pages/MarketPage'));
+const PoolPage = React.lazy(() => import('./pages/PoolPage'));
 const FarmPage = React.lazy(() => import('./pages/FarmPage'));
 const TradePage = React.lazy(() => import('./pages/TradePage'));
 const StakingPage = React.lazy(() => import('./pages/StakingPage'));
@@ -30,17 +30,17 @@ function App() {
 
           <Route path="/" element={<HomePage />} />
 
-          <Route path="/markets" element={<ComingSoon />} />
+          <Route path="/markets" element={<MarketPage />} />
 
-          <Route path="/pool" element={<ComingSoon />} />
+          <Route path="/pool" element={<PoolPage />} />
 
-          <Route path="/farm" element={<ComingSoon/>} />
+          <Route path="/farm" element={<FarmPage/>} />
 
-          <Route path="/trade" element={<ComingSoon />} />
+          <Route path="/trade" element={<TradePage />} />
 
-          <Route path="/stake" element={<ComingSoon />} />
+          <Route path="/stake" element={<StakingPage />} />
 
-          <Route path="/launchpad" element={<ComingSoon />} />
+          <Route path="/launchpad" element={<LaunchPadPage />} />
 
           <Route path="*" element={<Navigate to="/" />} />
 
