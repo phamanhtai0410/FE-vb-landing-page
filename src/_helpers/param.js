97368475c8e1dd7e74e3223beffb75d38536129a
Tra <tradaik1806@gmail.com>
2022-07-01 15:01:29 +0700

    update svg symbol

--- src/_helpers/param.js
+++ src/_helpers/param.js
@@ -1,7 +1,19 @@
-
-const refixTitle = 'VEBANK';
+const refixTitle = "VEBANK";
 
 export const routerHeaderTitle = {
-    home: refixTitle + ' - Home',
+  home: refixTitle + " - Home",
+};
 
-}
+export const svgSymbolConfig = {
+  r: 16,
+  opacity: "0.3",
+  strokeWidth: "8",
+  linearGradient: [
+    { offset: "0", stopColor: "#B9DDFF", stopOpacity: "1" },
+    { offset: "1", stopColor: "#12C9C9", stopOpacity: "0.3" },
+  ],
+  x1: "0",
+  y1: "0.5",
+  x2: "33.0585",
+  y2: "1.63369",
+};
