cc5cf12c308ccbe30d8d7074ae59fded267a122d
Tra <tradaik1806@gmail.com>
2022-07-19 11:43:13 +0700

    update navbar UI

--- src/pages/AddLiquidityPage.js
+++ src/pages/AddLiquidityPage.js
@@ -5,7 +5,7 @@ import ModalSelectToken from "../components/liquidity/AddLiquidity/selectToken/M
 const AddLiquidityPage = () => {
   return (
     <section className="box-borrows mx-auto bg-cover bg-center">
-      <div className="w-full h-[91vh] pb-9 flex items-center justify-center bg-content -z-50">
+      <div className="w-full h-full pb-9 min-h-screen flex items-center justify-center bg-content -z-50 pt-16">
         <AddLiquidity />
       </div>
       <ModalSelectToken />
