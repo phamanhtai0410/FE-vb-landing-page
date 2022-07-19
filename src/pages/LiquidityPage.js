6b8d99a612999ee129c3a562d524f066845a9013
Tra <tradaik1806@gmail.com>
2022-07-19 16:41:47 +0700

    update UI liquidity page

--- src/pages/LiquidityPage.js
+++ src/pages/LiquidityPage.js
@@ -6,7 +6,7 @@ import Liquidity from "../components/liquidity/HomeLiquidity";
 const LiquidityPage = () => {
   return (
     <section className="box-borrows mx-auto bg-cover bg-center">
-      <div className="w-full h-full pb-9 min-h-screen flex items-center justify-center bg-content -z-50">
+      <div className="w-full h-full pb-9 min-h-screen flex items-center justify-center bg-content -z-50 pt-24">
         <Liquidity />
       </div>
     </section>
